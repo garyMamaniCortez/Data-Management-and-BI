d35c4a72c5885c00d8a41fa223e7264fe69bf18a
garyMamaniCortez <mamanijhonny2210@gmail.com>
2026-05-04 16:59:51 -0400

    Feat: shippers tables for OLAP database

---
+++ NorthwindBI/NorthWindOLAP/Squema/Tables/staging/shippper.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [staging].[shipper] (
+    [ShipperID]        INT           NOT NULL,
+    [CompanyName]      NVARCHAR(40)  NOT NULL,
+    [Phone]            NVARCHAR(24)  NULL
+);
+GO
