b984d9f02fb0fa5b5d94a4f67514621225d04469
Erick-Quiroz <Quiroz.Erick@outlook.com>
2026-05-05 17:38:57 -0400

    Actulizar ruta , agregar stagingProduct

---
+++ NorthWindBI/NorthWindOLAP/Squema/Tables/staging/Product.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [staging].[product](
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nvarchar](40) NOT NULL,
+	[QuantityPerUnit] [nvarchar](20) NULL,
+	[UnitPrice] [money] NULL,
+	[UnitsInStock] [smallint] NULL,
+	[UnitsOnOrder] [smallint] NULL,
+	[ReorderLevel] [smallint] NULL,
+	[Discontinued] [bit] NOT NULL,
+	[CategoryID] [int] NULL,
+	[CategoryName] [nvarchar](15) NOT NULL,
+	[SupplierID] [int] NULL,
+	[SupplierCompanyName] [nvarchar](40) NOT NULL,
+	[SupplierContactName] [nvarchar](30) NULL,
+	[SupplierCity] [nvarchar](15) NULL,
+	[SupplierCountry] [nvarchar](15) NULL
+) ON [PRIMARY]
+GO
