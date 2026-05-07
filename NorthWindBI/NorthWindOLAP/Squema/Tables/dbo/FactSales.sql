96b845ea18986ce8ed4f2b5d7507aba125f72d45
marinsebastian <104607937+marinsebastian@users.noreply.github.com>
2026-05-06 23:35:33 -0400

    Add foreign keys to FactSales table
    Added foreign key constraints and ensured proper table structure for FactSales.

--- NorthWindBI/NorthWindOLAP/Squema/Tables/dbo/FactSales.sql
+++ NorthWindBI/NorthWindOLAP/Squema/Tables/dbo/FactSales.sql
@@ -1,31 +1,73 @@
-﻿-- Tabla de Hechos: FactSales
--- Tipo: OLAP / Data Warehouse
--- Descripcion: Tabla central de hechos del modelo estrella orientado al analisis de ventas
--- Contiene las metricas y medidas generadas a partir de las transacciones del sistema OLTP
--- Proposito: Facilitar consultas analiticas, reportes y procesos de Business Intelligence
--- Uso: Analisis de ventas, ingresos, descuentos, tendencias y rendimiento comercial
-
 CREATE TABLE [dbo].[FactSales](
-	[OrderDateSK] [int] NOT NULL,				-- Clave surrogate de la fecha de orden
-	[RequiredDateSK] [int] NULL,				-- Clave surrogate de la fecha requerida de entrega
-	[ShippedDateSK] [int] NULL,					-- Clave surrogate de la fecha de envio
-	[CustomerSK] [int] NOT NULL,				-- Clave surrogate del cliente
-	[ProductSK] [int] NOT NULL,					-- Clave surrogate del producto
-	[EmployeeSK] [int] NOT NULL,				-- Clave surrogate del empleado
-	[ShipperSK] [int] NOT NULL,					-- Clave surrogate de la empresa de envio
-	[OrderID] [int] NOT NULL,					-- ID original de la orden proveniente del OLTP
-	[ProductID] [int] NOT NULL,					-- ID original del producto proveniente del OLTP
-	[Quantity] [int] NOT NULL,					-- Cantidad total de productos vendidos
-	[UnitPrice] [money] NOT NULL,				-- Precio unitario del producto al momento de la venta
-	[Discount] [real] NOT NULL,					-- Porcentaje de descuento aplicado
-	[DiscountAmount] [money] NOT NULL,			-- Monto monetario descontado
-	[NetSales] [money] NOT NULL,				-- Venta neta despues del descuento
-	[Freight] [money] NULL,						-- Costo de envio asociado a la orden
-	[GrossRevenue] [money] NOT NULL,			-- Ingreso bruto antes de descuentos
+	[OrderDateSK] [int] NOT NULL,
+	[RequiredDateSK] [int] NULL,
+	[ShippedDateSK] [int] NULL,
+	[CustomerSK] [int] NOT NULL,
+	[ProductSK] [int] NOT NULL,
+	[EmployeeSK] [int] NOT NULL,
+	[ShipperSK] [int] NOT NULL,
+	[OrderID] [int] NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[UnitPrice] [money] NOT NULL,
+	[Discount] [real] NOT NULL,
+	[DiscountAmount] [money] NOT NULL,
+	[NetSales] [money] NOT NULL,
+	[Freight] [money] NULL,
+	[GrossRevenue] [money] NOT NULL,
  CONSTRAINT [PK_FactSales] PRIMARY KEY CLUSTERED 
 (
 	[OrderID] ASC,
 	[ProductID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[FactSales]  WITH CHECK ADD  CONSTRAINT [FK_FactSales_DimCustomer] FOREIGN KEY([CustomerSK])
+REFERENCES [dbo].[DimCustomer] ([CustomerSK])
+GO
+
+ALTER TABLE [dbo].[FactSales] CHECK CONSTRAINT [FK_FactSales_DimCustomer]
+GO
+
+ALTER TABLE [dbo].[FactSales]  WITH CHECK ADD  CONSTRAINT [FK_FactSales_DimDate_OrderDate] FOREIGN KEY([OrderDateSK])
+REFERENCES [dbo].[DimDate] ([DateSK])
+GO
+
+ALTER TABLE [dbo].[FactSales] CHECK CONSTRAINT [FK_FactSales_DimDate_OrderDate]
+GO
+
+ALTER TABLE [dbo].[FactSales]  WITH CHECK ADD  CONSTRAINT [FK_FactSales_DimDate_RequiredDate] FOREIGN KEY([RequiredDateSK])
+REFERENCES [dbo].[DimDate] ([DateSK])
+GO
+
+ALTER TABLE [dbo].[FactSales] CHECK CONSTRAINT [FK_FactSales_DimDate_RequiredDate]
+GO
+
+ALTER TABLE [dbo].[FactSales]  WITH CHECK ADD  CONSTRAINT [FK_FactSales_DimDate_ShippedDate] FOREIGN KEY([ShippedDateSK])
+REFERENCES [dbo].[DimDate] ([DateSK])
+GO
+
+ALTER TABLE [dbo].[FactSales] CHECK CONSTRAINT [FK_FactSales_DimDate_ShippedDate]
+GO
+
+ALTER TABLE [dbo].[FactSales]  WITH CHECK ADD  CONSTRAINT [FK_FactSales_DimEmployee] FOREIGN KEY([EmployeeSK])
+REFERENCES [dbo].[DimEmployee] ([EmployeeSK])
+GO
+
+ALTER TABLE [dbo].[FactSales] CHECK CONSTRAINT [FK_FactSales_DimEmployee]
+GO
+
+ALTER TABLE [dbo].[FactSales]  WITH CHECK ADD  CONSTRAINT [FK_FactSales_DimProduct] FOREIGN KEY([ProductSK])
+REFERENCES [dbo].[DimProduct] ([ProductSK])
+GO
+
+ALTER TABLE [dbo].[FactSales] CHECK CONSTRAINT [FK_FactSales_DimProduct]
+GO
+
+ALTER TABLE [dbo].[FactSales]  WITH CHECK ADD  CONSTRAINT [FK_FactSales_DimShipper] FOREIGN KEY([ShipperSK])
+REFERENCES [dbo].[DimShipper] ([ShipperSK])
+GO
+
+ALTER TABLE [dbo].[FactSales] CHECK CONSTRAINT [FK_FactSales_DimShipper]
 GO
