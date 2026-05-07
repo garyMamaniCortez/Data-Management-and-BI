5daf9659cfda5f587a26ee7103eac1e075a340ab
D4ag <diegoalvaradogarcia00@gmail.com>
2026-05-06 22:27:40 -0400

    Añadir tabla StagingCustomer.sql

---
+++ NorthWindBI/NorthWindOLAP/Squema/Tables/staging/Customer.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [staging].[customer](
+	[CustomerID] [nchar](5) NOT NULL,
+	[CompanyName] [nvarchar](40) NOT NULL,
+	[ContactName] [nvarchar](30) NULL,
+	[ContactTitle] [nvarchar](30) NULL,
+	[Address] [nvarchar](60) NULL,
+	[City] [nvarchar](15) NULL,
+	[Region] [nvarchar](15) NULL,
+	[PostalCode] [nvarchar](10) NULL,
+	[Country] [nvarchar](15) NULL,
+	[Phone] [nvarchar](24) NULL,
+	[Fax] [nvarchar](24) NULL,
+	[CustomerType] [nvarchar](100) NULL
+) ON [PRIMARY]
+GO
