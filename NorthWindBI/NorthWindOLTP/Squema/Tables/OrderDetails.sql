fecd7513006149aa2269a51fe7997cf7a958814c
marinsebastian <104607937+marinsebastian@users.noreply.github.com>
2026-05-06 23:37:12 -0400

    Add foreign keys to orderDetails table

--- NorthWindBI/NorthWindOLTP/Squema/Tables/OrderDetails.sql
+++ NorthWindBI/NorthWindOLTP/Squema/Tables/OrderDetails.sql
@@ -1,21 +1,46 @@
-﻿-- Tabla: OrderDetails
--- Tipo: OLTP / Transaccional
--- Descripcion: Tabla de detalle de productos asociados a cada orden del sistema ecommerce
--- Contiene informacion especifica de los productos vendidos en cada pedido
--- Proposito: Registrar cantidades, precios y descuentos aplicados por producto dentro de una orden
--- Uso: Procesamiento de ventas, calculo de ingresos y soporte para procesos ETL hacia el Data Warehouse
-
 CREATE TABLE [dbo].[OrderDetails](
-	[OrderID] [int] NOT NULL,				-- Referencia a la orden asociada
-	[ProductID] [int] NOT NULL,				-- Referencia al producto vendido
-	[UnitPrice] [money] NOT NULL,			-- Precio unitario del producto al momento de la venta
-	[Quantity] [smallint] NOT NULL,			-- Cantidad de unidades vendidas
-	[Discount] [real] NOT NULL,				-- Porcentaje de descuento aplicado al producto
-	[rowversion] [timestamp] NULL,			-- Control de concurrencia y soporte para ETL incremental
+	[OrderID] [int] NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[UnitPrice] [money] NOT NULL,
+	[Quantity] [smallint] NOT NULL,
+	[Discount] [real] NOT NULL,
+	[rowversion] [timestamp] NULL,
  CONSTRAINT [PK_OrderDetails] PRIMARY KEY CLUSTERED 
 (
 	[OrderID] ASC,
 	[ProductID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Orders] FOREIGN KEY([OrderID])
+REFERENCES [dbo].[Orders] ([OrderID])
+GO
+
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Orders]
+GO
+
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Products] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([ProductID])
+GO
+
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Products]
+GO
+
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [CK_OrderDetails_Discount] CHECK  (([Discount]>=(0) AND [Discount]<=(1)))
+GO
+
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [CK_OrderDetails_Discount]
+GO
+
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [CK_OrderDetails_Quantity] CHECK  (([Quantity]>(0)))
+GO
+
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [CK_OrderDetails_Quantity]
+GO
+
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [CK_OrderDetails_UnitPrice] CHECK  (([UnitPrice]>=(0)))
+GO
+
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [CK_OrderDetails_UnitPrice]
 GO
