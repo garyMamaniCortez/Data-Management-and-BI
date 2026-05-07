94877aff818fe00b92eda18bb4979fb4fccee5a8
Erick-Quiroz <93934190+Erick-Quiroz@users.noreply.github.com>
2026-05-06 23:28:46 -0400

    Refactor DimProduct table definition
    Removed comments and redundant lines in DimProduct table definition.

--- NorthWindBI/NorthWindOLAP/Squema/Tables/dbo/DimProducts.sql
+++ NorthWindBI/NorthWindOLAP/Squema/Tables/dbo/DimProducts.sql
@@ -1,26 +1,21 @@
-﻿-- Tabla de Dimension: DimProduct
--- Descripcion: Tabla denormalizada para analisis de productos en el OLAP
--- Contiene informacion de productos, categorias y proveedores integrada en una sola estructura
--- Proposito: Optimizar consultas analiticas y reportes de productos
--- Uso: Business Intelligence y analisis de ventas por producto
 
 CREATE TABLE [dbo].[DimProduct](
-	[ProductSK] [int] IDENTITY(1,1) NOT NULL,					-- Clave surrogada del producto
-	[ProductID] [int] NOT NULL,									-- ID original del producto
-	[ProductName] [nvarchar](40) NOT NULL,						-- Nombre del producto
-	[QuantityPerUnit] [nvarchar](20) NULL,						-- Cantidad por unidad
-	[UnitPrice] [money] NULL,									-- Precio unitario
-	[UnitsInStock] [smallint] NULL,								-- Unidades en stock
-	[UnitsOnOrder] [smallint] NULL,								-- Unidades en orden
-	[ReorderLevel] [smallint] NULL,								-- Nivel de reorden
-	[Discontinued] [bit] NOT NULL,								-- Indicador de producto discontinuado
-	[CategoryID] [int] NULL,									-- ID de categoria
-	[CategoryName] [nvarchar](15) NOT NULL,						-- Nombre de la categoria
-	[SupplierID] [int] NULL,									-- ID del proveedor
-	[SupplierCompanyName] [nvarchar](40) NOT NULL,				-- Nombre de la empresa proveedora
-	[SupplierContactName] [nvarchar](30) NULL,					-- Nombre del contacto del proveedor
-	[SupplierCity] [nvarchar](15) NULL,							-- Ciudad del proveedor
-	[SupplierCountry] [nvarchar](15) NULL,						-- Pais del proveedor
+	[ProductSK] [int] IDENTITY(1,1) NOT NULL,					
+	[ProductID] [int] NOT NULL,									
+	[ProductName] [nvarchar](40) NOT NULL,						
+	[QuantityPerUnit] [nvarchar](20) NULL,						
+	[UnitPrice] [money] NULL,									
+	[UnitsInStock] [smallint] NULL,								
+	[UnitsOnOrder] [smallint] NULL,								
+	[ReorderLevel] [smallint] NULL,								
+	[Discontinued] [bit] NOT NULL,								
+	[CategoryID] [int] NULL,									
+	[CategoryName] [nvarchar](15) NOT NULL,						
+	[SupplierID] [int] NULL,									
+	[SupplierCompanyName] [nvarchar](40) NOT NULL,				
+	[SupplierContactName] [nvarchar](30) NULL,					
+	[SupplierCity] [nvarchar](15) NULL,							
+	[SupplierCountry] [nvarchar](15) NULL,						
  CONSTRAINT [PK_DimProduct] PRIMARY KEY CLUSTERED 
 (
 	[ProductSK] ASC
