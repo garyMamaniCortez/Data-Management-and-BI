3e3c9b894e5872fec8da39d3e962681be705ad91
garyMamaniCortez <mamanijhonny2210@gmail.com>
2026-05-09 02:03:57 -0400

    feat: stored procedures

---
+++ Northwind/NorthWindOLAP/Squema/Programmability/Stored Procedures/DW_MergeDimShipper.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimShipper]
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    UPDATE dc
+    SET [CompanyName] = sc.[CompanyName],
+        [Phone]       = sc.[Phone]
+    FROM [dbo].[DimShipper]     dc
+    INNER JOIN [staging].[shipper] sc ON (dc.[ShipperID] = sc.[ShipperSK])  
+END
+GO
