36d83c2b2810b63a7df61c76151b0264fe313943
garyMamaniCortez <mamanijhonny2210@gmail.com>
2026-05-08 23:18:33 -0400

    feat: get shipper changes by row version store procedure

---
+++ Northwind/NorthWindOLTP/Squema/Programmability/Stored Procedures/GetShipperChangesByRowVersion.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[GetShipperChangesByRowVersion]
+(
+    @startRow BIGINT,
+    @endRow   BIGINT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    SELECT 
+        [ShipperID],
+        [CompanyName],
+        [Phone]
+    FROM [dbo].[Shippers]
+    WHERE [rowversion] > CONVERT(ROWVERSION, @startRow) 
+      AND [rowversion] <= CONVERT(ROWVERSION, @endRow)
+    ORDER BY [rowversion];
+    
+END
+GO
